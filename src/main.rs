f97396b834c621300e2b0adac8d1050b265e66a6
Michael Hampson <michael@flyingseamonsters.com>
2026-05-03 10:38:54 -0400

    Record source system in Track

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::env;
 
 #[derive(Debug, sqlx::FromRow)]
 struct Track {
+    source: Source,
     artist: String,
     album: String,
     track: String,
@@ -20,16 +21,18 @@ struct Cli {
     config: Option<String>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, sqlx::Type)]
 enum Source {
-    Plex,
+    Navidrome = 1,
+    Plex = 2,
 }
 
 fn get_source_query(source: &Source) -> &str {
     match source {
         Source::Plex => {
             r#"
-            select artist.title as artist,
+            select $1 as source,
+               artist.title as artist,
                album.title as album,
                track.title as track,
                track."index" as track_nbr,
@@ -59,14 +62,14 @@ fn get_source_query(source: &Source) -> &str {
             where track.library_section_id in (
                 select sl.library_section_id
                 from section_locations sl
-                /* this should be parameterized */
-                where sl.root_path = $1
+                where sl.root_path = $2
             )
               and s.rating is not null
 
             order by artist.title, album.title, track."index";
         "#
         }
+        Source::Navidrome => r#""#,
     }
 }
 
@@ -91,6 +94,7 @@ async fn main() -> anyhow::Result<()> {
     let source = get_source_query(&Source::Plex);
 
     let ratings: Vec<Track> = sqlx::query_as(source)
+        .bind(Source::Plex)
         .bind(&library)
         .fetch_all(&mut conn)
         .await?;
