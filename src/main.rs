07ea53af00449cf1c5febc3555f2ed319b5a1d30
mhampson31 <41697985+mhampson31@users.noreply.github.com>
2026-05-03 07:40:51 -0400

    Parameterize (#1)
    * Parameterize source library location for customization

--- src/main.rs
+++ src/main.rs
@@ -35,35 +35,35 @@ fn get_source_query(source: &Source) -> &str {
                track."index" as track_nbr,
                s.rating as rating,
                part.file
-   
+
             from metadata_items artist
 
             join metadata_items album
               on artist.id = album.parent_id
-  
+
             join metadata_items track
               on album.id = track.parent_id
-  
+
             join metadata_item_settings s
               on s.guid = track.guid
 
-             /* We don't need anything from media_items. 
-                It just lets us link metadata_items to media_parts 
+             /* We don't need anything from media_items.
+                It just lets us link metadata_items to media_parts
               */
             join media_items media
               on track.id = media.metadata_item_id
-  
+
             join media_parts part
               on part.media_item_id = media.id
-  
+
             where track.library_section_id in (
-                select sl.library_section_id 
-                from section_locations sl 
+                select sl.library_section_id
+                from section_locations sl
                 /* this should be parameterized */
-                where sl.root_path in ("/music")
+                where sl.root_path = $1
             )
               and s.rating is not null
-  
+
             order by artist.title, album.title, track."index";
         "#
         }
@@ -82,6 +82,7 @@ async fn main() -> anyhow::Result<()> {
         .unwrap();
 
     let source = settings.get::<String>("source").unwrap();
+    let library = settings.get::<String>("library").unwrap();
 
     println!("Getting ratings from {:?}", &source);
 
@@ -89,7 +90,10 @@ async fn main() -> anyhow::Result<()> {
 
     let source = get_source_query(&Source::Plex);
 
-    let ratings: Vec<Track> = sqlx::query_as(&source).fetch_all(&mut conn).await?;
+    let ratings: Vec<Track> = sqlx::query_as(source)
+        .bind(&library)
+        .fetch_all(&mut conn)
+        .await?;
     println!("{:#?}", ratings.len());
 
     conn.close().await?;
